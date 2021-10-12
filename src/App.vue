3a87e0afc5814864d4caa560ddde99c07b8deec9
Yakiv Shvets <yakiv.shvets@gmail.com>
2021-10-12 15:25:43 +0300

    Remove nav-bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <v-app>
-    <!-- <v-app-bar app color="primary" dark>
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/cart">Cart</router-link>
-      </div>
-    </v-app-bar> -->
-
     <v-main>
       <router-view />
     </v-main>
